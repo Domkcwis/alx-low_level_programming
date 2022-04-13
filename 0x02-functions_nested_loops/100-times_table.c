0772c192a887e3eda3d9b33b005f8aeb6fd375dc
Akpa Wisdom <domkcwis@gmail.com>
2022-04-13 02:31:32 +0000

    advance

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,17 +7,20 @@
  */
 void print_times_table(int n)
 {
-int num, nult, prod;
+int num, mult, prod;
 
 if (n >= 0 && n <= 15)
 {
 for (num = 0; num <= n; num++)
 {
 _putchar('0');
+
 for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
+
+prod = num * mult;
 
 if (prod <= 99)
 _putchar(' ');
