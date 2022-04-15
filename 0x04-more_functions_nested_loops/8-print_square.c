b705f2985f274507083e7bb836241952e0edcc94
Akpa Wisdom <domkcwis@gmail.com>
2022-04-15 23:00:40 +0000

    task 9

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,6 @@
 
 void print_square(int size)
 {
-
 int i, j;
 
 if (size > 0)
@@ -18,7 +17,7 @@ for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
-_putchar('a');
+_putchar('#');
 }
 _putchar('\n');
 }
