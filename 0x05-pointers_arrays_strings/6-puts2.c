fae6052c0e9a0a4dbdde102cf6f5b0276b6ea07c
Akpa Wisdom <80590563+Domkcwis@users.noreply.github.com>
2022-04-21 01:18:23 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,25 +5,15 @@
  * @str:pointer char
  * return:void
  */
-void puts2(chnr *str)
+void puts2(char *str)
 {
-<<<<<<< HEAD
-	int i, x;
+int i, x;
 i = 0;
 x = 0;
-	while (str[i++])
-	{
-	x++;
-	}
-=======
-  int i, x;
-i = 0;
-x = 0;
-  while (str[i++])
-  {
-  x++;
-  }
->>>>>>> d6b205dff9558fca77a89ed17cef730c78623378
+while (str[i++])
+{
+x++;
+}
 for (i = 0; i < x; i += 2)
 {
 _putchar(*(str + 1));
