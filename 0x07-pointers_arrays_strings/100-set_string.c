8306d0239bdfa9bfa0ef05cfc3f7f80c28c35f99
Akpa Wisdom <domkcwis@gmail.com>
2022-04-26 23:22:16 +0000

    Task 7

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * set_string - Sets the value of a pointer to a char
+ * @s: the value to modify
+ * @to: the value to assign
+ *
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+
+
+100-set_string.c
+#include "main.h"
+
+/**
  * set_string - sets the value of a pointer to a char
  * @s: content is to be copied
  * @to: string
@@ -9,5 +26,5 @@
 
 void set_string(char **s, char *to)
 {
- *s = to;
+*s = to;
 }
