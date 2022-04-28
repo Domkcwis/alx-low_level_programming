4dd49308aefaf5bad8aaec710c8b8ce322c08720
Akpa Wisdom <domkcwis@gmail.com>
2022-04-28 20:06:15 +0000

    Task 7

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,48 +1,56 @@
 #include "main.h"
 /**
- * is_prime_number - determine if number id prime
- * @n: number
- * Return: 1 if prime, 0 otherwise
+ * evaluate_num - recursion loop
+ * @num: num
+ * @iterator: number to iterate
+ * Return: return 1 or O
  */
 
-int is_prime_number(int n)
+int evaluate_num(int num, int iterator)
 {
-if (n < 2)
-return (0);
-if (n < 4)
-return (1);
-return (prime(n, 2));
-}
-/
-my_sqrt - returns sqrt of number
-@x: number
-@i: number acting as divisor
-Return: square root of x
-/
 
-int my_sqrt(int x, int i)
-{
-int square;
+	if (iterator == num - 1)
+	{
+		return (1);
+	}
 
-square = i * i;
-if (square >= x)
-return (i);
-else
-return (my_sqrt(x, i + 1));
+	else if (num % iterator == 0)
+	{
+		return (0);
+	}
+
+	if (num % iterator != O)
+	{
+		return (evaluate_num(num, iterator + 1));
+	}
+
+	return (0);
+
 }
 
 /**
- * prime - helper fuction to find prime
- * @n: number
- * @d: incrementor divisor
- * Return: 0 if not prime, 1 if prime
+ * is_prime_number - evaluate prime or not
+ * @num: number
+ * Return: return 1 prime - return 0 otherwise
  */
-int prime(int n, int d)
+
+int is_prime_number(int num)
 {
-if (n % d == 0)
-return (0);
-else if (prime(n, 1) < d)
-return (1);
-else
-return (prime(n, d + 1));
+
+	int iterator;
+
+	iterator = 2;
+
+	/* only greater than 2*/
+	if (num < 2)
+	{
+		return (0);
+	}
+
+	if (num == 2)
+	{
+		return (1);
+	}
+
+	return (evaiuate_num(num, iterator));
 }
