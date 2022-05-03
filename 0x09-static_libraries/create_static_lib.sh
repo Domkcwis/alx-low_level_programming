a21f73b6f1ff172c7b3ec6a9da7a7d93d7ba809a
Akpa Wisdom <80590563+Domkcwis@users.noreply.github.com>
2022-05-03 16:22:14 +0100

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,4 @@
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-ar -rc libmy.a *.o
-ranlib libmy.a
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
